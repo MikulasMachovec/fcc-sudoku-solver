45625432c7090987865edf55decff7ffe0447d60
Mikulas Machovec <mikimachovec@gmail.com>
2025-02-14 19:23:54 +0100

    validation on input in progress

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,30 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const pattern = /^[1-9.]+$/;
+      if(puzzleString.length !== 81){
+        throw new Error('Expected puzzle to be 81 characters long')
+      }
+
+       if(!pattern.test(puzzleString)) {
+        throw new Error('Invalid characters in puzzle')
+       }
+    return(true)
+  }
+
+  splitIntoChunks(puzzleString){
+    return puzzleString.match(/.{1,9}/g)
+  }
+
+  splitCoords(input){
+    const row = input[0].toUpperCase();
+    const column = parseInt(input.slice(1), 10,10)
+
+    return { row , column }
+  }
+
+  letterToNumber(letter){
+
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,52 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      const inputString = req.body.puzzle;
+      const coords = req.body.coordinate;
+      const value = req.body.value;
+      try{
+        const row = solver.splitCoords(value).row;
+        const column = solver.splitCoords(value).column;
+      if(!coords || !value || !inputString){
+        throw new Error('Required field(s) missing');
+      }else if( 1 < column ||  column > 9){
+        throw new Error('Invalid value') 
+      } else if('A' < row || row > 'I'){
+        throw new Error('Invalid coordinate') 
+      }
 
+
+      if (!inputString){
+        return res.json({ error: 'Required field missing' })
+      }
+
+      
+
+        if(solver.validate(inputString)) {
+          const chunks = solver.splitIntoChunks(inputString)
+          
+
+
+        }
+      } catch(error){
+        res.json({error: error.message})
+      }
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      const inputString = req.body.puzzle;
 
+      if (!inputString){
+        return res.json({ error: 'Required field missing' })
+      }
+      try{
+        if(solver.validate(inputString)) {
+          const chunks = solver.splitIntoChunks(inputString)
+          
+        }
+      } catch(error){
+        res.json({error: error.message})
+      }
     });
 };
